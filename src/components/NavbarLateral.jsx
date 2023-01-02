8b033b2108e8f9541b5e8b25fba23ea861c662a1
Emanuele <84925809+xdelmo@users.noreply.github.com>
2023-01-02 16:12:59 +0100

    FIX: navbar hover animation

--- src/components/NavbarLateral.jsx
+++ src/components/NavbarLateral.jsx
@@ -5,20 +5,20 @@ function NavbarLateral() {
     <nav>
       {/* <!-- Navigation --> */}
       <div className="relative flex justify-end flex-auto h-full text-xl font-bold uppercase font-secondary">
-        <div className="nav-lateral-link border-solid border-[1px] -right-[2rem] border-accent flex justify-center items-center  rounded-l-[1.25rem] z-[1] bg-primary max-w-[35px] relative navbar-link">
-          <span className="text-vertical-navbar">
-            <a href="">Bambino</a>
-          </span>
+        <div className="border-solid border-[1px] -right-[2rem] border-accent flex justify-center   items-center  rounded-l-[1.25rem] z-[1] bg-primary w-[35px] relative navbar-link ">
+          <a href="" className="text-vertical-navbar">
+            <span>Bambino</span>
+          </a>
         </div>
-        <div className="nav-lateral-link border-solid border-[1px] border-accent flex justify-center items-center  rounded-l-[1.25rem]  -right-[1rem] z-[2] bg-primary max-w-[35px]  relative  navbar-link">
-          <span className="text-vertical-navbar">
-            <a href="">Uomo</a>
-          </span>
+        <div className="border-solid border-[1px] border-accent flex justify-center items-center  rounded-l-[1.25rem]  -right-[1rem] z-[2] bg-primary w-[35px]  relative  navbar-link  ">
+          <a href="" className="text-vertical-navbar">
+            <span>Uomo</span>
+          </a>
         </div>
-        <div className="nav-lateral-link border-solid border-[1px] border-accent flex justify-center items-center  rounded-l-[1.25rem] z-[3] bg-primary max-w-[35px] navbar-link">
-          <span className="text-vertical-navbar">
-            <a href="">Donna</a>
-          </span>
+        <div className="border-solid border-[1px] border-accent flex justify-center items-center  rounded-l-[1.25rem] z-[3] bg-primary w-[35px]">
+          <a href="" className="text-vertical-navbar">
+            <span>Donna</span>
+          </a>
         </div>
       </div>
     </nav>
