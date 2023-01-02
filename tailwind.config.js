f333af6b68ec4b355965804106bf805580218c81
Emanuele <84925809+xdelmo@users.noreply.github.com>
2023-01-02 15:33:41 +0100

    FIX: default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       fontFamily: {
         main: ["Barlow Condensed", "sans-serif"],
-        secondary: ["Roboto", "sans-serif"],
+        secondary: ["Roboto", "serif"],
       },
     },
   },
