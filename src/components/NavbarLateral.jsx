5779aaf6fefba955254f2b17adfaa709d747419f
Emanuele <84925809+xdelmo@users.noreply.github.com>
2023-01-02 15:23:48 +0100

    FIX: relative responsive lateral navbar

--- src/components/NavbarLateral.jsx
+++ src/components/NavbarLateral.jsx
@@ -4,19 +4,19 @@ function NavbarLateral() {
   return (
     <nav>
       {/* <!-- Navigation --> */}
-      <div className="flex justify-end h-full text-xl font-bold uppercase font-secondary">
-        <div className="border-solid border-[1px] border-accent flex justify-center items-center overflow-hidden rounded-l-[1.25rem] z-[1] bg-primary relative -right-[4.5rem] navbar-link">
-          <span className="text-vertical-right-navbar">
+      <div className="relative flex justify-end flex-auto h-full text-xl font-bold uppercase font-secondary">
+        <div className="nav-lateral-link border-solid border-[1px] -right-[2rem] border-accent flex justify-center items-center  rounded-l-[1.25rem] z-[1] bg-primary max-w-[35px] relative navbar-link">
+          <span className="text-vertical-navbar">
             <a href="">Bambino</a>
           </span>
         </div>
-        <div className="border-solid border-[1px] border-accent flex justify-center items-center overflow-hidden rounded-l-[1.25rem] relative z-[2] bg-primary -right-7 navbar-link">
-          <span className="text-vertical-right-navbar">
+        <div className="nav-lateral-link border-solid border-[1px] border-accent flex justify-center items-center  rounded-l-[1.25rem]  -right-[1rem] z-[2] bg-primary max-w-[35px]  relative  navbar-link">
+          <span className="text-vertical-navbar">
             <a href="">Uomo</a>
           </span>
         </div>
-        <div className="border-solid border-[1px] border-accent flex justify-center items-center overflow-hidden rounded-l-[1.25rem] z-[3] bg-primary navbar-link">
-          <span className="text-vertical-right-navbar">
+        <div className="nav-lateral-link border-solid border-[1px] border-accent flex justify-center items-center  rounded-l-[1.25rem] z-[3] bg-primary max-w-[35px] navbar-link">
+          <span className="text-vertical-navbar">
             <a href="">Donna</a>
           </span>
         </div>
