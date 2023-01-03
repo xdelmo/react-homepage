d7f622465e2d5a712783fdb7dad9805a1c2aebf5
Emanuele <84925809+xdelmo@users.noreply.github.com>
2023-01-03 15:25:11 +0100

    FIX: removed MarqueeAnimation border top

--- src/components/MarqueeAnimation.jsx
+++ src/components/MarqueeAnimation.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function MarqueeAnimation() {
   return (
     <div class="marquee text-5xl marquee--hover-pause uppercase text-primaryDarker">
-      <span class="marquee__content inline w-fit ">
+      <span class="marquee__content inline w-fit font-thin ">
         Minimal*Etnico*Vintage*Classico*Moderno*Sportivo*Audace*Elegante*Casual*
       </span>
 
       {/* <!-- Mirrors the content above --> */}
-      <span class="marquee__content inline" aria-hidden="true">
+      <span class="marquee__content inline font-thin" aria-hidden="true">
         Minimal*Etnico*Vintage*Classico*Moderno*Sportivo*Audace*Elegante*Casual*
       </span>
     </div>
