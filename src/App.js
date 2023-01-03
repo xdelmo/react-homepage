be5662ea9e6eab776ba5b2268e26eae0462dad63
Emanuele <84925809+xdelmo@users.noreply.github.com>
2023-01-03 17:45:43 +0100

    UPDATE: added mobile basic structure

--- src/App.js
+++ src/App.js
@@ -12,6 +12,82 @@ function App() {
       {/* MOBILE APP */}
       <div className="min-h-screen md:hidden bg-primary">
         <DropdownMobile />
+        <main>
+          {/* CAROSELLO */}
+
+          {/* img */}
+          <figure className="mx-8 my-6">
+            {" "}
+            <img
+              src="https://source.unsplash.com/random/?jewelry"
+              alt="jewelry"
+              className="border-[1px] w-full img-main border-solid border-accent rounded-t-[100vmax] rounded-b-[100vmax] "
+            />{" "}
+          </figure>
+
+          {/* <---! BARRA NAVIGAZIONE ---> */}
+          <div className="flex items-center justify-between mx-5 mb-7">
+            {/* freccia sinistra */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+              />
+            </svg>
+
+            {/* icona categoria */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-11 h-11"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+
+            {/* freccia destra */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+              />
+            </svg>
+          </div>
+
+          {/* FOOTER */}
+          <div className="border-t border-solid border-accent grid-footer-mobile">
+            <div className="flex items-center justify-center grid-span-1-3">
+              <span className="font-semibold uppercase font-main text-[1.75rem]">
+                Lasciati consigliare
+              </span>
+            </div>
+            <div className="flex  items-center justify-end border-l-[1px] border-solid grid-span-3 border-accent">
+              <div className="w-12 h-12 m-4 rounded-full bg-accent"></div>
+            </div>
+          </div>
+        </main>
       </div>
       {/* DESKTOP APP */}
       <div className="hidden md:block">
